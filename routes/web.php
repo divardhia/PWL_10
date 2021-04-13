f1e36ed0178fcc759b6f4dedbc76c50376b99256
divardhia <divardhiass@gmail.com>
2021-04-13 16:12:23 +0700

    menambahkan fungsi cetak_pdf pada ArticleController

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\ArticleController;
 |
 */
 Route::resource('articles', ArticleController::class);
-Route::get('/article/cetak_pdf', ['articles'=>$articles]);
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
 
 Route::get('/', function () {
     return view('welcome');
